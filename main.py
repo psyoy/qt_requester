3b5a67b6455e5e49a61a60130a956f7fc3aaa9f0
Oydar <oydar1@outlook.com>
2025-02-08 12:01:32 +0300

    fix ui design

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel
+import json
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QTextEdit, QLabel
 import requests
 
-class MyWindow(QWidget):
+class GetWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("QT-Requester")
         self.setGeometry(100, 100, 500, 250)
 
-        self.label = QLabel("Введите url:", self)
-        self.label.setFixedWidth(200)
-        self.label.setWordWrap(True)
+        self.output = QTextEdit(self)
+        self.output.setFixedWidth(500)
+        self.output.setReadOnly(True)
+
+        self.label = QLabel("Enter url for request:", self)
         self.input = QLineEdit(self)
-        self.button = QPushButton("Отправить", self)
+        self.button = QPushButton("Send", self)
 
         layout = QVBoxLayout()
+        layout.addWidget(self.output)
         layout.addWidget(self.label)
         layout.addWidget(self.input)
         layout.addWidget(self.button)
@@ -26,19 +30,20 @@ class MyWindow(QWidget):
 
     def on_button_click(self):
         text = self.input.text()
-        json = req(text)
-        self.label.setText(f"Ответ:\n {json}")
+        json_line = req(text)
+        self.output.setText(f"{json_line}")
 
 
 def req(url):
     response = requests.get(url)
 
     if response.status_code == 200:
-        return response.json()
+        result = json.dumps(response.json(), indent=4)
+        return result
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MyWindow()
+    window = GetWindow()
     window.show()
     sys.exit(app.exec())
